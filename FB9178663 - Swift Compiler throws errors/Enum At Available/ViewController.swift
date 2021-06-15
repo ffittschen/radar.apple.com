a93f52a75b67a7559697fdead4931bac082905a3
Florian Fittschen <ffittschen@gmail.com>
2021-06-15 10:50:37 +0200

    Add new FB9178663

---
+++ FB9178663 - Swift Compiler throws errors/Enum At Available/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Enum At Available
+//
+//  Created by Florian Fittschen on 15.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
